52c3f2706aa2a51f93a513179c92d0ccfc43def1
VILLENEUVE <contact@michaelvilleneuve.fr>
2014-12-11 12:15:54 +0100

    Modif fichier bdd

--- calendrier/public/bdd.php
+++ calendrier/public/bdd.php
@@ -6,7 +6,27 @@ class Database {
 	private $dbname;
 	private $db;
 
+	public function listerSeminairesCal() {
+		$bd->query("SET NAMES 'utf8'");
+		$statement=$bd->prepare("SELECT *, DATE_FORMAT(date, '%m-%d-%Y') AS date FROM seminaire");
+		$statement->execute();
+		$results=$statement->fetchAll(PDO::FETCH_ASSOC);
+		echo json_encode($results);
+	}
+	public function listerSeminairesLs($debut,$fin) {
+		$bd->query("SET NAMES 'utf8'");
+        $select = $bd->query("SELECT * FROM seminaire WHERE date BETWEEN '$debut' AND '$fin' ORDER BY date ASC");
+		$select->setFetchMode(PDO::FETCH_OBJ);
+
+		while( $ligne = $select->fetch() ){
 	
+ 			echo '<h2>'.$ligne->titre.'</h2>';
+ 			echo "<p>Date: ".$ligne->date."</p>";
+ 			echo "<p>".$ligne->orateur."</p>";
+ 			echo "<p>".$ligne->lieu."</p>";
+ 			echo "<p>".$ligne->lien."</p>";
+		}
+	}
 	public function __construct()  {
 		$this->$server = 'localhost';
 		$this->$user = 'root';
