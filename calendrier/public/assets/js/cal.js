edc4ab6ce9842bc7e2dd0ba1f1102d13f771cf7a
VILLENEUVE <contact@michaelvilleneuve.fr>
2014-12-11 12:39:16 +0100

    ajouts fichiers + modifs js

---
+++ calendrier/public/assets/js/cal.js
@@ -0,0 +1,92 @@
+
+
+
+			$(function() {
+
+
+			$("div.right").mouseenter(function() {
+ 			 $("div.right").addClass('animated swing');
+			});
+
+			$("div.right").bind('oanimationend animationend webkitAnimationEnd', function() { 
+  				$("div.right").removeClass('animated swing');
+			});
+
+
+				
+
+				
+			
+				var transEndEventNames = {
+						'WebkitTransition' : 'webkitTransitionEnd',
+						'MozTransition' : 'transitionend',
+						'OTransition' : 'oTransitionEnd',
+						'msTransition' : 'MSTransitionEnd',
+						'transition' : 'transitionend'
+					},
+					transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+					$wrapper = $( '#custom-inner' ),
+					$calendar = $( '#calendar' ),
+
+					cal = $calendar.calendario( {
+						onDayClick : function( $el, $contentEl, dateProperties ) {
+
+							if( $contentEl.length > 0 ) {
+								showEvents( $contentEl, dateProperties );
+							}
+
+						},
+						caldata : codropsEvents,
+						displayWeekAbbr : true
+					} ),
+					$month = $( '#custom-month' ).html( cal.getMonthName() ),
+					$year = $( '#custom-year' ).html( cal.getYear() );
+
+				$( '#custom-next' ).on( 'click', function() {
+					cal.gotoNextMonth( updateMonthYear );
+				} );
+				$( '#custom-prev' ).on( 'click', function() {
+					cal.gotoPreviousMonth( updateMonthYear );
+				} );
+
+				
+
+
+				function updateMonthYear() {				
+					$month.html( cal.getMonthName() );
+					$year.html( cal.getYear() );
+				}
+
+				// just an example..
+				function showEvents( $contentEl, dateProperties ) {
+
+					hideEvents();
+					
+					var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"><h4>Evenements du '+ dateProperties.day + ' '+ dateProperties.monthname +' '+ dateProperties.year + '</h4></div>' ),
+						$close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
+
+					$events.append( $contentEl.html() , $close ).insertAfter( $wrapper );
+					
+					setTimeout( function() {
+						$events.css( 'top', '0%' );
+					}, 25 );
+
+				}
+				function hideEvents() {
+
+					var $events = $( '#custom-content-reveal' );
+					if( $events.length > 0 ) {
+						
+						$events.css( 'top', '100%' );
+						Modernizr.csstransitions ? $events.on( transEndEventName, function() { $( this ).remove(); } ) : $events.remove();
+
+					}
+
+				}
+			
+			});
+
+
+
+
+
---
+++ calendrier/public/index.php
@@ -0,0 +1,5 @@
+<?php 
+require_once 'bdd.php';
+$bdd = new Database();
+include 'vues/index.php';
+?>
