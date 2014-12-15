fcaccc2bcf02be7f2c490c5da02cd47a0ea7df80
VILLENEUVE <contact@michaelvilleneuve.fr>
2014-12-15 10:35:43 +0100

    Afficher lien qui si il existe

--- calendrier/vues/liste.php
+++ calendrier/vues/liste.php
@@ -6,6 +6,6 @@
 <h2><?php echo $v['titre']; ?></h2>
 <p>Orateur : <?php echo $v['orateur']; ?></p>
 <p>Lieu : <?php echo $v['lieu'];?></p>
- <p><a href="<?php echo $v['lien']; ?>">Visiter la page</a></p>
+ <?php if (!empty($v['lien'])) { ?><p><a href="<?php echo $v['lien']; ?>">Visiter la page</a></p><?php } ?>
 </div>
 <?php } ?>
