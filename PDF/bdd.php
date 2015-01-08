69fdc01b283666149e570df5b29fe1a77002b10f
jeremy <jybe33@hotmail.fr>
2015-01-08 17:51:17 +0100

    Avancement PDF

---
+++ PDF/bdd.php
@@ -0,0 +1,46 @@
+<?php 
+class Database {
+	private $server;
+	private $user;
+	private $password;
+	private $dbname;
+	private $bd;
+	public function __construct()  {
+		$this->server = 'localhost';
+		$this->user = 'root';
+		$this->password = 'root';
+		$this->dbname = 'seminaires';
+		try
+		{
+		$this->bd = new PDO("mysql:host=$this->server;dbname=$this->dbname", "$this->user", "$this->password");
+		}
+		catch(Exception $e)
+		{
+        die('Erreur : '.$e->getMessage());
+		}
+	}
+	public function listerSeminairesCal() {
+		$reponse = $this->bd->prepare("SELECT * FROM seminaire");
+		$reponse->execute();
+		while ($donnees = $reponse->fetch())
+		{
+			?>
+
+'<?php echo date("m-d-Y", strtotime($donnees['date'])); ?>' : '<span><h2>Titre : <?php echo $donnees['titre']; ?></h2><p>Orateur : <?php echo $donnees['orateur']; ?></p><p>Lieu : <?php echo $donnees['lieu']; ?></p><img src="../public/assets/images/logo-<?php echo $donnees['labo']; ?>.png"/></span>',<?php
+		}
+	}
+	public function listerSeminairesLs($debut,$fin) {
+		$this->bd->query("SET NAMES 'utf8'");
+        $select = $this->bd->query("SELECT * FROM seminaire WHERE date BETWEEN '$debut' AND '$fin' ORDER BY date ASC");
+		$select->setFetchMode(PDO::FETCH_OBJ);
+		while( $ligne = $select->fetch() ){
+	
+ 			echo '<h2>'.$ligne->titre.'</h2>';
+ 			echo "<p>Date: ".$ligne->date."</p>";
+ 			echo "<p>".$ligne->orateur."</p>";
+ 			echo "<p>".$ligne->lieu."</p>";
+ 			echo "<p>".$ligne->lien."</p>";
+		}
+	}
+ }
+ ?>
--- PDF/index.php
+++ PDF/index.php
@@ -1,38 +1,110 @@
 <?php
+
 require('fpdf.php');
 
 class PDF extends FPDF
 {
 function Header()
 {
-    // Logo
-    $this->Image('logo.png',62,-2,90);
 
-    // Rectangle
+//////////HEADER/////////////
+    // Logo
+    $this->Image('logo.png',62,-6,90);
+
+
+
+//////////CORPS CHAPITRE/////
+
+
+$pdo = new PDO('mysql:host=localhost;dbname=seminaires', 'root', 'root');
+    $pdo->query("SET NAMES 'utf8'");
+    $select = $pdo->query("SELECT * FROM seminaire");
+
+
+$y_default = 30;
+$x_default = 51;
+
+$x_jour = 33;
+$y_jour = 35;
+$x_mois = 32;
+$y_mois = 40;
+$y_orateur = 35;
+$y_lieu = 40;
+$y_labo = 45;
+$y_lien = 44;
+
+$x1_carre = 28;
+$x2_carre = 27;
+$largeur_carre = 17;
+$hauteur_carre = 17;
+
+while( $ligne = $select->fetch(PDO::FETCH_OBJ) ){
+
+			//CARRE
+			$this->SetFillColor(91,91,91);
+    		$this->Rect($x1_carre,$x2_carre,$largeur_carre,$hauteur_carre, 'F');
+    		//DECALAGE CARRE
+    		$x2_carre = $x2_carre + 25;
+
+    		//JOUR DATE
+			$this->SetFont('Arial', 'B', 18);
+			$this->SetTextColor(251,251,251);
+			$this->Text($x_jour,$y_jour,(date("d", strtotime($ligne->date))));
+
+			//MOIS DATE
+			$this->SetFont('Arial', 'B', 12);
+			$this->SetTextColor(251,251,251);
+			setlocale(LC_TIME, 'fra_fra');
+			$this->Text($x_mois,$y_mois,ucfirst(strftime('%b',strtotime($ligne->date))));
+
+			//INFORMTIONS
+			$this->SetFont('Arial', '', 11);
+			$this->SetTextColor(91,91,91);
+			$this->Text($x_default,$y_default,$ligne->titre);
+			$this->Text($x_default,$y_orateur,$ligne->orateur);
+			$this->Text($x_default,$y_lieu,$ligne->lieu);
+			$this->SetFont('Arial', 'I', 9);
+			$this->Text($x_default,$y_lien,$ligne->lien);
+
+			$y_default = $y_default + 25;
+			$y_orateur = $y_orateur + 25;
+			$y_jour = $y_jour + 25;
+			$y_mois = $y_mois + 25;
+			$y_lieu = $y_lieu + 25;
+			$y_labo = $y_lieu + 25;
+			$y_lien = $y_lien + 25;
+
+}
+
+
+
+
+
+    // Ligne
+    $this->Line(45,186,165,186,'F',false);
+    // Texte
+    $this->SetFont('Arial','',14);
+    $this->SetTextColor(91,91,91);
+    $this->Text(96,194,utf8_decode("DIVERS"));
+
+
+
+
+//////////FOOTER////////////
+	// Rectangle
     $this->SetFillColor(91,91,91);
-    $this->Rect(62,28,90,22, 'DF');
+    $this->Rect(65,245,80,16, 'DF');
 
     // Texte
     $this->SetTextColor(255,255,255);
-    $this->SetFont('helvetica','B',20);
-    $this->Text(70,36,utf8_decode("Congrès Annuel SFP :"));
-    $this->Text(90,47,utf8_decode("Août 2016"));
+    $this->SetFont('helvetica','B',16);
+    $this->Text(76,251,utf8_decode("Congrès Annuel SFP :"));
+    $this->Text(92,259,utf8_decode("Août 2016"));
 
-}
-
-
-function CorpsChapitre()
-{
-	
-	
-}
-
-
-function Footer()
-{
 	// Ligne
     $this->Line(0,266,210,266,'F',false);
     // Informations
+    $this->SetTextColor(91,91,91);
     $this->SetFont('Arial','',12);
 	$this->MultiCell(0,523,utf8_decode('Société Française de Physique'), 0, 'C');
 	$this->MultiCell(0,-513,'33 Rue de Croulebarbe, 75013 Paris', 0, 'C');
